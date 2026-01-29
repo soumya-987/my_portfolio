a1ff6e22ede86b323e64f6aea0577a50d44eaf2c
Soumya Jain <soumya09062006@gmail.com>
2026-01-29 07:46:15 +0530

    fixing bugs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+  plugins: [
+    '@tailwindcss/postcss',
+    'autoprefixer', // It's highly recommended to include this for browser compatibility
+  ],
 };
 
 export default config;
